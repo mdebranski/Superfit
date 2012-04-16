d8493f84b73b041ccad9f27989f05e1f4236e357
Jeremy Haile <jhaile@gmail.com>
2012-04-16 15:44:16 -0400

    Initial project commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'foreman'
+gem 'haml'
+gem 'jquery-rails'
+gem 'pg'
+gem 'sqlite3'
+gem 'twitter-bootstrap-rails'
+gem 'unicorn'
+
+group :development do
+  gem 'heroku'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superfit::Application.config.secret_token = '73d1f92563c03d471363f824f7f976f1f520916635a1ce140f2a311b16b53385435666136c9c61333b2b6ffd184d6ff0526e5eb67e8238520d3b180c4ec46c22'
