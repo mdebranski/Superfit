d5f04cba61574090a4a3c5f7a121e35608b028d4
Jeremy Haile <jhaile@gmail.com>
2012-06-30 14:32:53 -0500

    Support heroku's DATABASE_URL

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,13 @@
 
 require 'csv'
 
-props = YAML.load(File.read(File.join(Rails.root, 'config', 'database.yml')))[Rails.env]
-props[:adapter] = :postgres # Sequel requires postgres instead of postgresql
-DB = Sequel.connect(props)
+if ENV['DATABASE_URL']
+  DB = Sequel.connect(ENV['DATABASE_URL'])
+else
+  props = YAML.load(File.read(File.join(Rails.root, 'config', 'database.yml')))[Rails.env]
+  props[:adapter] = :postgres # Sequel requires postgres instead of postgresql
+  DB = Sequel.connect(props)
+end
 
 scoring_method_map = {"For Time" => 'for_time', "Pass/Fail" => 'pass_fail', "AMRAP  (rounds)" => 'rounds', "For time" => 'for_time', "Max Reps" => 'max_reps' }
 
