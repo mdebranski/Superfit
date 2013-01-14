7559675309d520a7692b2b1b20920d34526b4808
Jeremy Haile <jhaile@gmail.com>
2013-01-13 20:31:16 -0500

    Fix for compass on heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Superfit::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w(superfit.js superfit.css)
 
+  # Required for devise on heroku (https://github.com/plataformatec/devise)
+  config.assets.initialize_on_precompile = false
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
